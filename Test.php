07f98fce22503cf638005df77ddd20e6ba47221a
ghie21 <10112250.ecp@email.unikom.ac.id>
2015-03-07 12:26:45 +0700

    Checkpoint ke-1

---
+++ Test.php
@@ -0,0 +1,14 @@
+<html>
+<head><title>Latihan Repo</title></head>
+<body>
+
+<center><h2>Kelompok RPL</h2></center>
+
+<?php
+
+		echo "<center>10112250 - Egi Cahyo P</center>";
+	
+?>
+
+</body>
+</html>
